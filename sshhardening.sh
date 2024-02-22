f5634da574a3f7b0e2defecfb59964d27efb1bbc
justanotherscript <94018782+justanotherscript@users.noreply.github.com>
2024-02-22 18:49:43 +0100

    Update sshhardening.sh

--- sshhardening.sh
+++ sshhardening.sh
@@ -2,6 +2,11 @@
 
 #!/usr/bin/env bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root." 1>&2
+	exit 1
+fi
+
 if [ ! -e "/etc/ssh/sshd_config.d" ]; then
   mkdir /etc/ssh/sshd_config.d
 fi
