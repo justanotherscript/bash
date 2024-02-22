9cee11c4d729c791bb66139c92bf4893e975cd3c
justanotherscript <94018782+justanotherscript@users.noreply.github.com>
2024-02-22 18:53:30 +0100

    Update nginxhardening.sh

--- nginxhardening.sh
+++ nginxhardening.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ $EUID -ne 0 ]]; then
+	echo "This script must be run as root." 1>&2
+	exit 1
+fi
+
 sudo cp -a /etc/nginx/nginx.conf /etc/nginx.conf.Back &&
 apt update && apt install nginx-extras -y &&
 
